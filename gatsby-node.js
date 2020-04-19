f11c65cb44bab7fb295de93a552330976c34ecf5
rickylau <rickylaufitness@gmail.com>
2020-04-19 11:43:36 -0400

    create-edit: create postLayout component, edit gatsby node postLayout template

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,38 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require('path')
 
-// You can delete this file if you're not using it
+ exports.createPages = ({graphql, actions}) => {
+     const {createPage} = actions
+     const postLayoutTemplate = path.resolve('./src/components/postLayout.js')
+
+     return graphql(`
+       query blogSlugsQuery {
+         allMarkdownRemark {
+           edges {
+             node {
+               frontmatter {
+                 slug
+               }
+             }
+           }
+          }
+        }
+     `).then(results => {
+         if (results.errors) {
+             throw results.errors
+         }
+         results.data.allMarkdownRemark.edges.forEach(({node}) => {
+             createPage({
+                 path: node.frontmatter.slug,
+                 component: postLayoutTemplate,
+                 context: {
+                     slug: node.frontmatter.slug
+                 }
+             })
+         })
+     })
+ }
+
+
---
+++ src/components/postLayout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Layout from './layout'
+
+const PostLayout = () => {
+    
+
+    return (
+        <>
+        <Layout>
+        <h1>Post Layout</h1>
+        </Layout>
+        </>
+    )
+}
+
+PostLayout.propTypes = {
+
+}
+
+export default PostLayout
+

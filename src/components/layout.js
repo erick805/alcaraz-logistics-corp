3542828aa9e92adcf4d81a3fcea77a22937e6518
rickylau <rickylaufitness@gmail.com>
2020-04-21 11:18:26 -0400

    update: new square transparent logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import Img from 'gatsby-image'
+// import Img from 'gatsby-image'
 import { Spring } from 'react-spring/renderprops'
 import { useStaticQuery, graphql } from "gatsby"
 import styled from 'styled-components'

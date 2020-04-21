be5d3338000a90d9cd8ba2b7326c7ace2f3c84d4
rickylau <rickylaufitness@gmail.com>
2020-04-21 17:33:40 -0400

    edits: move spring to index NavButton

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,11 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, {useState} from "react"
+import React from "react"
 import PropTypes from "prop-types"
 // import { Spring } from 'react-spring/renderprops'
 import { useStaticQuery, graphql } from "gatsby"
 import styled from 'styled-components'
-import {useSpring, animated} from 'react-spring'
 
 // Components
 
@@ -26,12 +25,9 @@ const MainLayout = styled.main`
   margin-left: 7rem;
   margin-top: 3rem;
 `
-const Layout = ({ children, location }) => {
+const Layout = ({ children}) => {
 
-const [state, toggle] = useState(true)
-const { x } = useSpring({from: {x: 0}, x: state ? 1 : 0, config: { duration: 1000 }})
-
-  const data = useStaticQuery(graphql`
+  useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -46,19 +42,8 @@ const { x } = useSpring({from: {x: 0}, x: state ? 1 : 0, config: { duration: 100
   return (
     <>
       <MainLayout>
-        <div onClick={() => toggle(!location.pathname)}>
-          <animated.div
-          style={{
-            opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
-            transform: x
-              .interpolate({
-                range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
-                output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
-              })
-              .interpolate(x => `scale(${x})`)
-          }}>
+        <div>
           {children}
-          </animated.div>
         </div>
       </MainLayout>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React, {useState} from "react"
 import { Link } from "gatsby"
 
 // Components
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import styled from 'styled-components'
+import {useSpring, animated} from 'react-spring'
 
 // Assets
 import Logo from "../components/nonStretchedLogo"
@@ -17,15 +18,34 @@ export const NavButton = styled.div`
   // border-radius: 3px;
 `;
 
-const IndexPage = ({location}) => (
+const IndexPage = ({location}) => {
+
+const [state, toggle] = useState(true)
+const { x } = useSpring({from: {x: 0}, x: state ? 1 : 0, config: { duration: 1000 }})
+
+return (
   <Layout location={location} >
     <Logo/>
     <SEO title="Home" />
     <h1>Alcaraz Logistics Corporation</h1>
     <NavButton >
-      <Link to="/about/" >About US</Link>
+      <animated.div
+            role="button" tabIndex={0} onClick={() => toggle(!location.pathname)} onKeyDown={() => toggle(!location.pathname)}
+            style={{
+              opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
+              transform: x
+                .interpolate({
+                  range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+                  output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
+                })
+                .interpolate(x => `scale(${x})`)
+            }}>
+        <Link to="/about/" >About US</Link>
+      </animated.div>
     </NavButton>
   </Layout>
+
 )
+}
 
 export default IndexPage

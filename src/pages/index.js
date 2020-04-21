60e048d5487ae5da8e4c1e8eb3f1366a71b60c12
rickylau <rickylaufitness@gmail.com>
2020-04-21 08:53:07 -0400

    final boilerplate edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,28 @@ import { Link } from "gatsby"
 // Components
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styled from 'styled-components'
 
 // Assets
 import Logo from "../components/image"
+
+export const NavButton = styled.button`
+  background: "white";
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #5ce1e6;
+  border-radius: 3px;
+`;
 
 const IndexPage = ({location}) => (
   <Layout location={location}>
     <Logo/>
     <SEO title="Home" />
     <h1>Alcaraz Logistics Corporation</h1>
-    <Link to="/page-2/">Go to page 2</Link>
+    <NavButton primary>
+      <Link to="/about/" >About US</Link>
+    </NavButton>
   </Layout>
 )
 

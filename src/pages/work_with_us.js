aeaf722bc0253722c05e72aa05bcf460ab198757
rickylau <rickylaufitness@gmail.com>
2020-04-25 15:34:44 -0400

    add hidden form-name

--- src/pages/work_with_us.js
+++ src/pages/work_with_us.js
@@ -21,7 +21,8 @@ const work_with_us = ({ intl }) => (
       data-netlify="true" 
       data-netlify-honeypot="bot-field"
     >
-      <input type="hidden" name="bot-field" value="contact" />
+      <input type="hidden" name="bot-field" />
+      <input type="hidden" name="form-name" value="contact" />
       <p>
         <label>Your Name: <input type="text" name="name" /></label>   
       </p>

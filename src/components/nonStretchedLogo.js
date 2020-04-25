250e072591c44338f600862f623cdd8a4fd1ac29
rickylau <rickylaufitness@gmail.com>
2020-04-24 21:21:34 -0400

    edit: Img component to no style

--- src/components/nonStretchedLogo.js
+++ src/components/nonStretchedLogo.js
@@ -27,7 +27,7 @@ const NonStretchedLogo = () => {
   }
 `)
 
-  return <Img fixed={data.file.childImageSharp.fixed} style={{display: 'block', marginLeft: 'auto', marginRight: 'auto' }}/>
+  return <Img fixed={data.file.childImageSharp.fixed} />
 }
 
 export default NonStretchedLogo

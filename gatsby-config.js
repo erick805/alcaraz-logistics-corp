2f009ded8c2ea899f540fd0224d921f604637a5d
rickylau <rickylaufitness@gmail.com>
2020-04-25 12:47:59 -0400

    add netlify headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,8 @@ module.exports = {
         icon: `src/images/logo.png` // This path is relative to the root of the site.
       }
     },
-    `gatsby-transformer-remark`
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-netlify`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
